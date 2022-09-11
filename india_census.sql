2dce65d1438452674e8ff2aa30ede9ec680dddd7
aminzl <94546423+aminzl@users.noreply.github.com>
2022-09-12 00:16:48 +0100

    fotmating data into csv files and initiating sql exploratory

---
+++ india_census.sql
@@ -0,0 +1,12 @@
+-- first of all lets create our data base and use it
+create database india_census;
+use india_census;
+
+-- next we import the data sets and explore them
+select * from dataset1;
+describe dataset1;
+
+select * from dataset2;
+describe dataset2;
+
+
