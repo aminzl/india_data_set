e12b9114a66ee0411a272ec2fababdf96cfbc410
aminzl <94546423+aminzl@users.noreply.github.com>
2022-09-12 03:25:27 +0100

    weadded some code in sql

--- india_census.sql
+++ india_census.sql
@@ -5,8 +5,38 @@ use india_census;
 -- next we import the data sets and explore them
 select * from dataset1;
 describe dataset1;
+/* we need to set district and state as a composed primery key
+select distinct district from dataset1 634 rows that means there are duplicated values;
+select distinct state from dataset1 34 rows that means there are duplicated value ;
+select distinct district,state from dataset1 ; 640 rows that means all rows are unique */
 
 select * from dataset2;
 describe dataset2;
+/* we need to set the primery key from the table above dataset1 
+as a foreign key in dataset2 */
+
+
+
+-- number of entities (rows) into our data sets :
+select count(*) as nomber_of_rows from dataset1 ; -- the result is 640.
+select count(*) as nomber_of_rows from dataset2 ; -- the result is 640.
+
+
+-- dataset for jharkhand and bihar
+select * from dataset1 where state in ('jharkhand','bihar'); -- 62 rows returned
+
+
+-- calculating the total population of india from table dataset2 
+select sum(population) as total_population from dataset2 ; -- '1 210 854 977'
+
+-- next we want to know the average growth of india
+select concat(format(avg(growth)*100,2),'%') as average_growth from dataset1; -- the average growth is'19.25%'
+/* we calculated the average growth using avg() function and we passed the column growth as its argument
+then we multiped the result by 100 to find the percentage , after that we used the format() function to round the
+number with two decimal places , lastely using concat() function we added the '%' sign the the result */
+
+
+
+
 
 
