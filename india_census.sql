0662c5bee7c458343c5b0f4957b1d940e98ea419
aminzl <94546423+aminzl@users.noreply.github.com>
2022-09-12 03:38:04 +0100

    ''

--- india_census.sql
+++ india_census.sql
@@ -32,8 +32,9 @@ select sum(population) as total_population from dataset2 ; -- '1 210 854 977'
 -- next we want to know the average growth of india
 select concat(format(avg(growth)*100,2),'%') as average_growth from dataset1; -- the average growth is'19.25%'
 /* we calculated the average growth using avg() function and we passed the column growth as its argument
-then we multiped the result by 100 to find the percentage , after that we used the format() function to round the
-number with two decimal places , lastely using concat() function we added the '%' sign the the result */
+then we multiped the result by 100 to convert the result to percentage , after that we used the format() 
+function to round the number with two decimal places , lastely using concat() function we added 
+the '%' sign the the result */
 
 -- now we want to know the average growth of each state
 select state , concat(format(avg(growth)*100,2),'%') as average_growth_per_state from dataset1 group by state;
