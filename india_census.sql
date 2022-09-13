0f0348498580bc920f479575eba1f3d754564225
aminzl <94546423+aminzl@users.noreply.github.com>
2022-09-13 03:21:23 +0100

    completing sql file

--- india_census.sql
+++ india_census.sql
@@ -40,7 +40,66 @@ the '%' sign the the result */
 select state , concat(format(avg(growth)*100,2),'%') as average_growth_per_state from dataset1 group by state;
 
 
+-- next we want to know the average sex ratio in each state and order it from the highest to the lowest
+select state , round(avg(sex_ratio),0) as average_sex_ratio from dataset1
+group by state order by average_sex_ratio desc ;
+
+
+/*moving on lets calculate the average literacy rate and display the states
+where the average literacy is greater than 90 */
+select state , round(avg(literacy),0) as average_literacy_rate from dataset1
+group by state having average_literacy_rate>90 order by average_literacy_rate desc;
+
+
+-- top 3 state showing highest average growth ratio
+select state , round(avg(growth)*100,0) as average_growth_rate from dataset1
+group by state order by average_growth_rate desc limit 3;
+
+-- bottom 3 state showing lowest average sex ratio
+select state , round(avg(sex_ratio),0) as average_sex_ratio from dataset1
+group by state order by average_sex_ratio asc limit 3;
+
+
+/*next lets say we want to display top and bottom 3 states in literacy rate state in 
+in the same table using union command*/
+
+drop table if exists top_state;
+create table top_state (
+state varchar(255),
+topstate float
+);
+insert into top_state
+select state , round(avg(literacy),0) as average_literacy_ratio from dataset1
+group by state order by average_literacy_ratio desc;
+select * from top_state order by topstate desc limit 3;
+
+
+
+drop table if exists bottom_state;
+create table bottom_state (
+state varchar(255),
+bottomstate float
+);
+insert into bottom_state
+select state , round(avg(literacy),0) as average_literacy_ratio from dataset1
+group by state order by average_literacy_ratio desc;
+select * from bottom_state order by bottomstate asc limit 3;
+
+
+-- combining the result of two queries in one table using union
+select * from (select * from top_state order by topstate desc limit 3) as a
+union 
+select * from (select * from bottom_state order by bottomstate asc limit 3) as b;
+
+
+-- states starting with letter a or b
+select distinct state from dataset1 where lower(state) like 'a%'or lower(state) like 'b%';
+
+-- states starting with letter a and ending with letter m 
+select distinct state from dataset1 where lower(state) like 'a%'and lower(state) like '%m';
 
 
 
 
+
+ 
